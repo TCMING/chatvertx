33819c8f078192f13a8e91993b62b58a4b434f17
ubuntu <ubuntu@localhost.localdomain>
2021-08-15 19:56:06 +0800

    vertx rest url 使用

--- src/main/java/com/chat/Main.java
+++ src/main/java/com/chat/Main.java
@@ -2,13 +2,80 @@ package com.chat;
 
 import com.chat.controller.UserController;
 import io.vertx.core.Vertx;
+import io.vertx.core.AbstractVerticle;
+import io.vertx.core.http.HttpServer;
+import io.vertx.core.json.Json;
+import io.vertx.core.json.JsonObject;
+import io.vertx.ext.web.Router;
+import io.vertx.ext.web.RoutingContext;
+import io.vertx.ext.web.handler.BodyHandler;
 
-public class Main {
+import java.util.HashMap;
+import java.util.Map;
+
+
+public class Main extends AbstractVerticle {
+
+    private UserService userService;
 
     public static void main(String[] args){
-        Vertx vertx = Vertx.vertx();
 
-        vertx.deployVerticle(UserController.class.getName());
+	//集群部署
+	//VertxOptions options = new VertxOptions();
+	//Vertx.clusteredVertx(options, res -> {
+	//	if (res.succeeded()) {
+	//		res.result().deployVerticle(new HttpVerticle());
+	//	}
+	//}
+
+        Vertx vertx = Vertx.vertx();
+	vertx.deployVerticle(new RestServer());
+	//userService = new UserService()
     }
 
+
+    @Override
+    public void start() throws Exception {
+        HttpServer server = vertx.createHttpServer();
+
+        Router router = Router.router(vertx);
+        router.route().handler(BodyHandler.create());
+
+        router.post("/user").blockingHandler(this::addUser);
+	router.get("/userLogin").blockingHandler(this::userlogin);
+	router.get("/user/{username}").handler(this::getUserInfo);
+
+        server.requestHandler(router);
+        server.listen(8080);
+    }
+
+    private void addUser(RoutingContext ctx) {
+
+    }
+
+    private void userlogin(RoutingContext ctx) {
+
+    }
+
+    private void getUserInfo(RoutingContext ctx) {
+
+    }
+
+
+
+
+
+
 }
+
+
+
+
+
+
+
+
+
+
+
+r
