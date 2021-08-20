899f0c63ef539030a820cd3cdd358fd99fa4d2a7
zhouyi.thomas <zhouyi.thomas@bigo.sg>
2021-08-20 17:56:51 +0800

    接口先摆上去

--- src/main/java/com/chat/Main.java
+++ src/main/java/com/chat/Main.java
@@ -23,17 +23,68 @@ class ChatServer extends AbstractVerticle {
     private MessageService messageService;
 
     @Override
-    public void start() throws Exception{
+    public void start() throws Exception {
         HttpServer server = vertx.createHttpServer();
 
         Router router = Router.router(vertx);
         router.route().handler(BodyHandler.create());
 
-        router.post("/user").handler(this::addUser);
         router.get("/test").handler(this::getTest);
+
+        // user
+        router.post("/user").blockingHandler(this::addUser);
+        router.get("/userLogin").blockingHandler(this::userlogin);
+        router.get("/user").blockingHandler(this::getUserInfo);
+
+        // message
+        router.post("/message/send").blockingHandler(this::msgSend);
+        router.post("/message/retrieve").blockingHandler(this::getMsgList);
+
+        //room
+        router.post("/room").blockingHandler(this::room);
+        router.put("/room/id/enter").blockingHandler(this::roomEnter);
+        router.put("/room/roomLeave").blockingHandler(this::roomLeave);
+        router.get("/room").blockingHandler(this::roomIdList);
+        router.get("/roomList").blockingHandler(this::roomList);
+        router.get("/room/id/users").blockingHandler(this::roomUserList);
+
+
 
         server.requestHandler(router);
         server.listen(8080);
+    }
+
+    private  void roomList(RoutingContext routingContext) {
+
+    }
+
+    private void roomUserList(RoutingContext routingContext) {
+
+    }
+
+    private void roomIdList(RoutingContext routingContext) {
+
+    }
+
+    private void getMsgList(RoutingContext routingContext) {
+    }
+
+    private void roomLeave(RoutingContext routingContext) {
+    }
+
+    private void roomEnter(RoutingContext routingContext) {
+    }
+
+    private void room(RoutingContext routingContext) {
+    }
+
+    private void msgSend(RoutingContext routingContext) {
+    }
+
+    private void getUserInfo(RoutingContext routingContext) {
+    }
+
+    private void userlogin(RoutingContext routingContext) {
     }
 
     private void getTest(RoutingContext routingContext) {
