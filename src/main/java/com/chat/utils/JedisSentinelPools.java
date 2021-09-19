d265d9eb7f9086a2551a35626e3ea1976b446a2f
yuanshanhongye <382675942@qq.com>
2021-09-20 00:57:57 +0800

    redis 读写分离

--- src/main/java/com/chat/utils/JedisSentinelPools.java
+++ src/main/java/com/chat/utils/JedisSentinelPools.java
@@ -24,7 +24,7 @@ public class JedisSentinelPools {
     //如果赋值为-1，则表示不限制，如果pool已经分配了maxActive个jedis实例，则此时pool的状态为exhausted(耗尽)
     private static Integer MAX_TOTAL = 100;
     //控制一个pool最多有多少个状态为idle(空闲)的jedis实例，默认值是8
-    private static Integer MAX_IDLE = 8;
+    private static Integer MAX_IDLE = 10;
     //等待可用连接的最大时间，单位是毫秒，默认值为-1，表示永不超时。
     //如果超过等待时间，则直接抛出JedisConnectionException
     private static Integer MAX_WAIT_MILLIS = 10000;
@@ -68,6 +68,7 @@ public class JedisSentinelPools {
         config.setTestWhileIdle(TEST_WHILE_IDLE);
         config.setTestOnReturn(TEST_ON_RETURN);
         localPool = new JedisPool(config, "127.0.0.1", 6379, TIMEOUT);
+//        localPool = new JedisPool(config, "47.94.19.223", 6379, TIMEOUT);
     }
 
     private static void initPool() {
